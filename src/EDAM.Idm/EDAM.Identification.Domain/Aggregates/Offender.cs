031834583a2fa9084fbd145fd4bbb92f1c57359c
Yaser Adel Mehraban <adel.mehrban@gmail.com>
2024-01-05 08:16:52 +1000

    Added the identification solution

---
+++ src/EDAM.Idm/EDAM.Identification.Domain/Aggregates/Offender.cs
@@ -0,0 +1,13 @@
+﻿using EDAM.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDAM.Identification.Domain.Aggregates
+{
+    public class Offender : IDomainAggregateRoot
+    {
+    }
+}
---
+++ src/EDAM.Idm/EDAM.Identification.Domain/EventHandlers/PassengerIdentifiedEventHandler.cs
@@ -0,0 +1,18 @@
+﻿using EDAM.Core.Interfaces;
+using EDAM.Identification.Domain.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDAM.Identification.Domain.EventHandlers
+{
+    public class PassengerIdentifiedEventHandler : IDomainEventHandler<PassengerIdentifiedEvent>
+    {
+        public Task Handle(PassengerIdentifiedEvent notification, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
