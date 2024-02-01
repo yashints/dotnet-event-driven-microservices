e6b093c895a4578ee90e7a42d95ef293e0f6e15a
Yaser Adel Mehraban <adel.mehrban@gmail.com>
2024-02-02 08:16:52 +1000

    Added the passenger solution

---
+++ src/EDAM.Passg/EDAM.Passenger.Domain/Aggregates/Passenger.cs
@@ -0,0 +1,13 @@
+﻿using EDAM.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDAM.Passenger.Domain.Aggregates
+{
+    public class Passenger : IDomainAggregateRoot
+    {
+    }
+}
