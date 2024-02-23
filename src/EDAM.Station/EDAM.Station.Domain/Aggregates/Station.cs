1417900e60532a883b795381ca59937778a6475d
Yaser Adel Mehraban <adel.mehrban@gmail.com>
2024-02-23 11:16:52 +1000

    Added the station solution

---
+++ src/EDAM.Station/EDAM.Station.Domain/Aggregates/Station.cs
@@ -0,0 +1,15 @@
+﻿using EDAM.Core.Interfaces;
+using EDAM.Station.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDAM.Station.Domain.Aggregates
+{
+    public class Station : IDomainAggregateRoot
+    {
+        public StationInfo StationData { get; private set; } = default!;
+    }
+}
